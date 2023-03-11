f42f9513c78634049d2544d99b8186c254d367cd
Jonathas Souza <jonathas.js@gmail.com>
2023-03-11 20:39:30 -0300

    Fix Preloader

--- public/js/main.js
+++ public/js/main.js
@@ -57,9 +57,9 @@
     .on("load", function () {
       $(".preloader").fadeOut(800, "linear");
       var ptPage = $(".animated-sections");
-      if (ptPage[0]) {
+      //if (ptPage[0]) {
         PageTransitions.init({ menu: "ul.main-menu" });
-      }
+      //}
     })
     .on("resize", function () {
       mobileMenuHide();
