c5c146435b3c287b1e815dfed723033a0e0015fc
Jonathas Souza <jonathas.js@gmail.com>
2023-03-16 18:57:42 -0300

    Fixed animação da home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,9 +8,9 @@ export default function Home({ data,name }) {
           <div className="col-sm-12 col-md-12 col-lg-12">
             <div className="title-block">
               <h2> {name} </h2>
-              <div className="owl-carousel text-rotation">
+              <div className="text-animation">
                 {data.content.occupations.map((o,index)=> (
-                <div key={index} className="item">
+                <div key={index} className="item ">
                   <div className="sp-subtitle">{o}</div>
                 </div>
                 ))}
